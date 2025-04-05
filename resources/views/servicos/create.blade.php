9954d360c0d3879dd2271f5939785632f5d37de1
IsabelleCabriotti <isahcabriotti@gmail.com>
2025-04-04 23:45:47 -0300

    servicos

--- resources/views/servicos/create.blade.php
+++ resources/views/servicos/create.blade.php
@@ -3,18 +3,18 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Novo Produto</title>
+    <title>Novo Serviço</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!--<link rel="stylesheet" href="{{ asset('css/app.css') }}"> arrumar a mensagem de sucesso depois-->
   </head>
   <body class="container">
-    <h1>Novo Produto</h1>
+    <h1>Novo Serviço</h1>
     
     <form method="post" action="/produtos">
         @csrf
                         
         <div class="mb-3">
-            <label for="nome" class="form-label">Informe o nome:</label>
+            <label for="nome" class="form-label">Informe o nome do serviço:</label>
             <input type="text" id="nome" name="nome" class="form-control" required="">
         </div>
     
@@ -29,25 +29,14 @@
         </div>
     
         <div class="mb-3">
-            <label for="estoque" class="form-label">Informe o estoque:</label>
-            <input type="text" id="estoque" name="estoque" class="form-control" required="">
-        </div>
-    
-        <div class="mb-3">
-            <label for="categoria" class="form-label">Selecione a categoria:</label>
-            <select id="categoria_id" name="categoria_id" class="form-select" required="">
-                @foreach ($categorias as $c)
-                    <option value="{{ $c->id }}">
-                        {{ $c->nome }}
-                    </option>
-                @endforeach
-            </select>
+            <label for="tempo" class="form-label">Informe o tempo de realização:</label>
+            <input type="text" id="tempo" name="tempo" class="form-control" required="">
         </div>
     
     <button type="submit" class="btn btn-primary">Enviar</button>
     @if (session('sucesso'))
         <div class="alert alert-success">
-            <p class="mensagem-sucesso">{{ session('successo') }} Produto adicionado com sucesso!</p>
+            <p class="mensagem-sucesso">{{ session('successo') }} Serviço adicionado com sucesso!</p>
         </div>
     @endif
 </form>
